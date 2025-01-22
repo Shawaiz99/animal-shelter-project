9be8d937ab15eb33bfa7e38b66836d52ff52b70d
Shawaiz <shawaizy2k@gmail.com>
2025-01-22 11:56:17 -0500

    Added Flex to the buttons and add some spacing for style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,19 @@
 function Hero() {
-    return (
-      <div className="bg-light text-dark py-5">
-        <div className="container text-center">
-          <h1 className="display-2">Adopt Today!</h1>
-          <p className="lead">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illo est ut laborum debitis quasi atque quis officiis adipisci repellendus.
-          </p>
-          <button className="btn btn-primary">Learn More</button>
+  return (
+    <div className="bg-light text-dark py-5">
+      <div className="container text-center">
+        <h1 className="display-2">Adopt Today!</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illo
+          est ut laborum debitis quasi atque quis officiis adipisci repellendus.
+        </p>
+        <div className="button-container d-flex justify-content-center ">
+          <button className="btn btn-primary mx-1">Learn More</button>
+          <button className="btn btn-primary mx-1">Adopt Today!</button>
         </div>
       </div>
-    );
-  }
-  
-  export default Hero;
+    </div>
+  );
+}
+
+export default Hero;
